4ad992bd4e8c3530e25f7e33e3dfb93b4c517627
shafayet-N <neutronos.u@gmail.com>
2025-10-29 02:04:59 -0700

    Update POLog.tsx

--- src/components/POLog.tsx
+++ src/components/POLog.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLanguage } from '@/contexts/LanguageContext';
-import LanguageTransitionWrapper from './LanguageTransitionWrapper';
+
 
 const POLog: React.FC = () => {
   const [showPassword, setShowPassword] = useState(false);
